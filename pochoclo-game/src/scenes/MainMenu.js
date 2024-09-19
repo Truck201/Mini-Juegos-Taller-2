4ba2f3785fd418865efd01472f5dca7be846bba2
Truck201 <joaquingandini22@gmail.com>
2024-09-19 01:06:12 -0700

    screen sleep menu feature

--- pochoclo-game/src/scenes/MainMenu.js
+++ pochoclo-game/src/scenes/MainMenu.js
@@ -1,98 +1,169 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+    this.movingIcon = null;
+    this.allicons = null;
+  }
+  init() {
+    this.cameras.main.fadeIn(500, 0, 0, 0);
+    this.allicons = [];
+  }
+  create() {
+    let width = this.scale.width; //Definir la mitad del Ancho
+    let height = this.scale.height; //Definir la mitad del Alto
+    let miImagen; //Definir una Variable en la escena
+
+    //Title
+    this.background = this.add
+      .sprite(width / 2, height / 2, "l-opacidad")
+      .setScale(2);
+
+    //Button
+    miImagen = this.add.text(100, 240, "Play", {
+      fontSize: "33px",
+      fill: "#fff",
+    });
+    //this.add.image(width / 2, height / 2, "").setScale(0.15);
+    miImagen.setInteractive();
+
+    //Button Animations Hover, Down, Out
+    miImagen.on("pointerover", () => {
+      // Cambia el tamaño de la imagen al pasar el mouse
+      miImagen.setScale(1.1);
+      console.log("on");
+    });
+
+    miImagen.on("pointerout", () => {
+      // Cambia el tamaño de la imagen al pasar el mouse
+      miImagen.setScale(1);
+      console.log("off");
+    });
+
+    miImagen.on("pointerdown", () => {
+      miImagen.setScale(1.2); // Vuelve al tamaño original
+      console.log("active");
+      // this.add.image(width / 2, height / 2, "").setScale(0.37); //Explosión
+      this.time.addEvent({
+        delay: 900, // demora 1 segundo en iniciar
+        loop: true,
+        callback: () => {
+          this.transitionToNextScene(); //Llama la escena Main
+        },
+      });
+    });
+
+    // Control de inactividad
+    this.inactivityTimer = this.time.addEvent({
+      delay: 11000, // 1 minuto
+      callback: this.showIconScreen,
+      callbackScope: this,
+      loop: true,
+    });
+
+    // Detectar movimiento del mouse
+    this.input.on("pointermove", () => {
+      this.resetInactivityTimer();
+    });
+
+    let options; // Botón de opciones
+
+    //Button
+    options = this.add.text(100, 310, "Options", {
+      fontSize: "27px",
+      fill: "#fff",
+    });
+    //this.add.image(width / 2, height / 2, "").setScale(0.15);
+    options.setInteractive();
+
+    //Button Animations Hover, Down, Out
+    options.on("pointerover", () => {
+      // Cambia el tamaño de la imagen al pasar el mouse
+      options.setScale(1.1);
+    });
+
+    options.on("pointerout", () => {
+      // Cambia el tamaño de la imagen al pasar el mouse
+      options.setScale(1);
+    });
+
+    options.on("pointerdown", () => {
+      options.setScale(0.9); // Vuelve al tamaño original
+      console.log("active");
+      // this.add.image(width / 2, height / 2, "").setScale(0.37); //Explosión
+      this.time.addEvent({
+        delay: 300, // demora 1 segundo en iniciar
+        loop: true,
+        callback: () => {
+          this.toOptionsScene(); //Llama la escena Main
+        },
+      });
+    });
+  }
+
+  resetInactivityTimer() {
+    // Si hay objetos en el array allicons, eliminarlos
+    if (this.allicons.length > 0) {
+      this.allicons.forEach((icon) => {
+        icon.destroy(); // Destruir cada objeto en el array
+      });
+      this.allicons = []; // Vaciar el array después de destruir los objetos
     }
 
-    create() {
-        let width = this.scale.width; //Definir la mitad del Ancho
-        let height = this.scale.height; //Definir la mitad del Alto
-        let miImagen; //Definir una Variable en la escena
-    
-        //Title
-        this.background = this.add.sprite(width / 2, height / 2, "l-opacidad").setScale(2);
-    
-        //Button
-        miImagen = this.add.text(100, 240, 'Play', { fontSize: '33px', fill: '#fff' });
-        //this.add.image(width / 2, height / 2, "").setScale(0.15);
-        miImagen.setInteractive();
-    
-        //Button Animations Hover, Down, Out
-        miImagen.on("pointerover", () => {
-          // Cambia el tamaño de la imagen al pasar el mouse
-          miImagen.setScale(1.1);
-          console.log("on");
-        });
-    
-        miImagen.on("pointerout", () => {
-          // Cambia el tamaño de la imagen al pasar el mouse
-          miImagen.setScale(1);
-          console.log("off");
-        });
-    
-        miImagen.on("pointerdown", () => {
-          miImagen.setScale(1.2); // Vuelve al tamaño original
-          console.log("active");
-          // this.add.image(width / 2, height / 2, "").setScale(0.37); //Explosión
-          this.time.addEvent({
-            delay: 900, // demora 1 segundo en iniciar
-            loop: true,
-            callback: () => {
-              this.transitionToNextScene(); //Llama la escena Main
-            },
-          });
-        });
-    
-    
-        let options; // Botón de opciones
-    
-        //Button
-        options = this.add.text(100, 310, 'Options', { fontSize: '27px', fill: '#fff' });
-        //this.add.image(width / 2, height / 2, "").setScale(0.15);
-        options.setInteractive();
-    
-        //Button Animations Hover, Down, Out
-        options.on("pointerover", () => {
-          // Cambia el tamaño de la imagen al pasar el mouse
-          options.setScale(1.1);
-        });
-    
-        options.on("pointerout", () => {
-          // Cambia el tamaño de la imagen al pasar el mouse
-          options.setScale(1);
-        });
-    
-        options.on("pointerdown", () => {
-          options.setScale(0.9); // Vuelve al tamaño original
-          console.log("active");
-          // this.add.image(width / 2, height / 2, "").setScale(0.37); //Explosión
-          this.time.addEvent({
-            delay: 300, // demora 1 segundo en iniciar
-            loop: true,
-            callback: () => {
-              this.toOptionsScene(); //Llama la escena Main
-            },
-          });
-        });
-    
-      }
-    
-    
-      toOptionsScene() {
-        this.scene.start("opcionesScene"); //Ir a escena Opciones
-      }
-    
-      transitionToNextScene() {
-        // Crear el efecto de zoom out
-        this.cameras.main.zoomTo(0.4179, 1300); // Reducir el zoom en 1 segundo (1000 ms)
-    
-        // Esperar un poco antes de iniciar la siguiente escena
-        this.time.delayedCall(1500, () => {
-          this.scene.start("Game1vs1"); //Ir a escena Main
-        });
-      }
-    
-    }
+    this.cameras.main.setAlpha(1); // Restaurar la opacidad normal
+
+    // Reiniciar el temporizador de inactividad
+    this.inactivityTimer.reset({
+      delay: 11000,
+      callback: this.showIconScreen,
+      callbackScope: this,
+      loop: true,
+    });
+  }
+
+  showIconScreen() {
+    let width = this.scale.width;
+    let height = this.scale.height;
+
+    // Crear el ícono que se moverá
+    this.movingIcon = this.add
+      .image(
+        Phaser.Math.Between(0, width), // Posición inicial X aleatoria
+        Phaser.Math.Between(0, height), // Posición inicial Y aleatoria
+        "logo"
+      )
+      .setScale(0.5);
+
+    // Movimiento del ícono por la pantalla con posiciones y duraciones aleatorias
+    this.tweens.add({
+      targets: this.movingIcon,
+      x: Phaser.Math.Between(0, width), // Posición final X aleatoria
+      y: Phaser.Math.Between(0, height), // Posición final Y aleatoria
+      duration: Phaser.Math.Between(2000, 6000), // Duración aleatoria del movimiento
+      repeat: -1,
+      yoyo: true,
+      ease: "Sine.easeInOut", // Movimiento más suave
+    });
+
+    // Guardar el ícono en el array
+    this.allicons.push(this.movingIcon);
+
+    // Agregar opacidad a la pantalla
+    this.cameras.main.setAlpha(0.5);
+  }
+
+  toOptionsScene() {
+    this.scene.start("opcionesScene"); //Ir a escena Opciones
+  }
+
+  transitionToNextScene() {
+    // Crear el efecto de zoom out
+    this.cameras.main.zoomTo(0.4179, 1300); // Reducir el zoom en 1 segundo (1000 ms)
+
+    // Esperar un poco antes de iniciar la siguiente escena
+    this.time.delayedCall(1500, () => {
+      this.scene.start("Game1vs1"); //Ir a escena Main
+    });
+  }
+}
--- pochoclo-game/src/scenes/Preloader.js
+++ pochoclo-game/src/scenes/Preloader.js
@@ -1,44 +1,82 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
-        super('Preloader');
-    }
+export class Preloader extends Scene {
+  constructor() {
+    super("Preloader");
+  }
 
-    init ()
-    {
+  init() {
+    //  A simple progress bar. This is the outline of the bar.
+    //this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
+    //const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+    //this.load.on('progress', (progress) => {
+    //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
+    //bar.width = 4 + (460 * progress);
+    //});
+  }
 
-        //  A simple progress bar. This is the outline of the bar.
-        //this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+  preload() {
+    //this.load.image('logo', 'logo.png');
+    //  Main Menu
+    // LOGO
+    this.load.image("logo", "../public/assets/menu/logo.png");
+    //  Game
+    //  Personajes
+    this.load.image("mimbo", "../public/assets/mimbo/mimbo.png");
+    this.load.image("luho", "../public/assets/luho/luho.png");
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        //const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    //   Pochoclos
+    this.load.image("pochoclo", "../public/assets/barra/static-pororo.png");
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        //this.load.on('progress', (progress) => {
+    this.load.spritesheet(
+      "pochoclo-anims",
+      "../public/assets/anims/plop-pororo.png",
+      {
+        frameWidth: 90,
+        frameHeight: 114,
+      }
+    );
 
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            //bar.width = 4 + (460 * progress);
+    //   Opacidad
+    this.load.image("l-opacidad", "../public/assets/opacidad.png");
 
-        //});
-    }
+    //  Tienda
+    this.load.spritesheet(
+      "pororo-tienda",
+      "../public/assets/tienda/idle-pororo.png",
+      {
+        frameWidth: 48,
+        frameHeight: 51,
+      }
+    );
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
-        //this.load.setPath('assets');
+    this.load.spritesheet(
+      "caramelo",
+      "../public/assets/tienda/caramelo-azul.png",
+      {
+        frameWidth: 60,
+        frameHeight: 72,
+      }
+    );
 
-        //this.load.image('logo', 'logo.png');
-    }
+    this.load.spritesheet("pizza", "../public/assets/tienda/zapi.png", {
+      frameWidth: 72,
+      frameHeight: 69,
+    });
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
+    // Partículas
+    this.load.image("flare", "../public/assets/particles/particles.png");
+    this.load.image("flare2", "../public/assets/particles/particles2.png");
+    this.load.image("flare3", "../public/assets/particles/particles3.png");
+  }
 
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
-    }
+  create() {
+    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+    //  For example, you can define global animations here, so we can use them in other scenes.
+
+    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    this.scene.start("MainMenu");
+  }
 }
